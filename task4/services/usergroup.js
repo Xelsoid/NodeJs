f20820067b82d66f45f021389b0b7615e3bb75c1
Artsiom_Reutovich <artemrevtovich@gmail.com>
2020-10-27 12:10:53 +0300

    [task4] fixed addUserToGroup method

--- task4/services/usergroup.js
+++ task4/services/usergroup.js
@@ -4,10 +4,7 @@ module.exports = class UserService {
   static async addUsersToGroup (groupId, usersId) {
     const user = await User.findByPk(usersId);
     const group = await Group.findByPk(groupId);
-    await group.addUsers(user).then(() => {
-      return 'userGroupAdded';
-    }).catch(() => {
-      return 'userGroupNotAdded';
-    });
+    const result = await group.addUsers(user);
+    return result ? 'userGroupAdded' : 'userGroupNotAdded';
   };
 };
