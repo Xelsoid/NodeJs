a3cbd9ecc1938c0cb5c34dace07a4bbab5e6e26a
Artsiom_Reutovich <artemrevtovich@gmail.com>
2020-11-23 18:07:53 +0300

    [task7] added initial files

---
+++ task7/constants/errorResponses.js
@@ -0,0 +1,54 @@
+const GOBACKLINK = '<a href="/">Go back</a>';
+const RESPONSES = {
+  createUser: {
+    created: `The user created. ${GOBACKLINK}`,
+    exists: `The user already exist. Try another name. ${GOBACKLINK}`,
+  },
+  deleteUser: {
+    deleted: `The user deleted. ${GOBACKLINK}`,
+    notExists: `The user doesn't exist or wrong password entered. ${GOBACKLINK}`,
+  },
+  updateUser: {
+    userUpdated: `The user info updated. ${GOBACKLINK}`,
+    userNotUpdated: `The user doesn't exist or the password doesn't match. Cannot update user. ${GOBACKLINK}`,
+  },
+  findUsers: {
+    usersList: `Please find user's list. ${GOBACKLINK}`,
+    usersNotFound: `The password doesn't match. Cannot update user. ${GOBACKLINK}`,
+  },
+  getUser: {
+    usersExists: `Information about user by ID. ${GOBACKLINK}`,
+    usersNotFound: `User with such ID not found. ${GOBACKLINK}`,
+  },
+
+  createGroup: {
+    created: `The Group created. ${GOBACKLINK}`,
+    exists: `The Group already exist. Try another name. ${GOBACKLINK}`,
+    updated: `The Group updated.  ${GOBACKLINK}`,
+  },
+  updateGroup: {
+    groupUpdated: `The Group info updated. ${GOBACKLINK}`,
+    groupNotUpdated: `The Group doesn't exist. Cannot update Group. ${GOBACKLINK}`,
+  },
+  deleteGroup: {
+    deleted: `The Group deleted. ${GOBACKLINK}`,
+    notExists: `The Group doesn't exist. ${GOBACKLINK}`,
+  },
+  getGroup: {
+    groupExists: `Information about user by ID. ${GOBACKLINK}`,
+    groupNotFound: `User with such ID not found. ${GOBACKLINK}`,
+  },
+  getGroups: {
+    groupsList: `Please find group's list. ${GOBACKLINK}`,
+    groupsNotFound: `Groups not found. ${GOBACKLINK}`,
+  },
+  addUserToGroups: {
+    userGroupAdded: `User added to group. ${GOBACKLINK}`,
+    userGroupNotAdded: `Smth gone wrong. ${GOBACKLINK}`,
+  },
+  login: {
+    logged: `Login is successful. ${GOBACKLINK}`,
+  }
+};
+
+module.exports = RESPONSES;
---
+++ task7/data-access/index.js
@@ -0,0 +1,43 @@
+const Sequelize = require('sequelize');
+const UserModel = require('../models/user');
+const GroupModel = require('../models/group');
+const { MockedUsers, MockedGroups } = require('../mockedData');
+
+const sequelize = new Sequelize('postgres', 'postgres', '1234', {
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  define: {
+    "createdAt": "createdat",
+    "updatedAt": "updatedat"
+  }
+});
+
+const User = UserModel(sequelize, Sequelize);
+const Group = GroupModel(sequelize, Sequelize);
+User.belongsToMany(Group, { through: 'UserGroup', unique: false });
+Group.belongsToMany(User, { through: 'UserGroup', unique: false });
+
+sequelize.sync({ force: true }).then(async () => {
+  await sequelize.transaction(async (t) => {
+    return Group.create(MockedGroups, {transaction: t});
+  });
+
+  await sequelize.transaction(async (t) => {
+    return User.bulkCreate(MockedUsers, {transaction: t});
+  }).then((users) => {
+    Group.findAll().then((groups) => {
+      groups.forEach((group) => {
+        group.setUsers(users).then(console.log('users! added'))
+      });
+    });
+  });
+});
+
+module.exports = {
+  User, Group
+};
